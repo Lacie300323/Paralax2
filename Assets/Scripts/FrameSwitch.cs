130916082304deb68c1cc3e2542bbbff62490656
NovaYin <laciepark300323@hotmail.com>
2019-11-26 00:16:10 +0100

    Adding all the files

---
+++ Assets/Scripts/FrameSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameSwitch : MonoBehaviour
+{
+    public GameObject frame1;
+    public GameObject frame2;
+
+    private void Start()
+    {
+        frame2.active= false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (frame1.active == true)
+        {
+            frame1.SetActive(false);
+            frame2.SetActive(true);
+        }
+        else if(frame1.active == false)
+        {
+            frame1.SetActive(true);
+            frame2.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public CharacterController2D controller; //controls crouching, jumping, walking and checking if the player is touching hte ground
+    float horizontalMove = 0f;
+    public float movementSpeed = 15f;
+
+    //Checking for player movements jumping and crouching aside from walking
+    bool jump = false;
+    bool crouch = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * movementSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            //Debug.Log("derp"); 
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }  
+    }
+
+    void FixedUpdate()
+    {
+        //move character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+       
+        jump = false;
+
+        //transform.Translate(new Vector2(horizontalMove * Time.deltaTime, 0));
+    }
+}
